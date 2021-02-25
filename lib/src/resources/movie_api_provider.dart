4e3ab0f7f7b1b6619abe2f551f76829f33f486f2
Sergey <fangooner.ars@gmail.com>
2021-02-25 12:43:55 +0100

    init

---
+++ lib/src/resources/movie_api_provider.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+import 'package:http/http.dart' show Client;
+import 'dart:convert';
+import '../constants.dart';
+import '../models/item_model.dart';
+import '../models/trailer_model.dart';
+
+class MovieApiProvider {
+  Client client = Client();
+  final _apiKey = apiKey;
+  final _baseUrl = 'http://api.themoviedb.org/3/movie';
+
+  Future<ItemModel> fetchMovieList() async {
+    print("entered");
+    final response = await client.get("$_baseUrl/popular?api_key=$_apiKey");
+    print(response.body.toString());
+    if (response.statusCode == 200) {
+      return ItemModel.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load post');
+    }
+  }
+
+  Future<TrailerModel> fetchTrailer(int movieId) async {
+    final response =
+        await client.get("$_baseUrl/$movieId/videos?api_key=$_apiKey");
+    if (response.statusCode == 200) {
+      return TrailerModel.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load trailers');
+    }
+  }
+}
